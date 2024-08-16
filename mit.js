016c7a14371dbd5440645667675360251e60083b
Dipak6700 <dpkmahato135790@gmail.com>
2024-08-16 10:29:40 +0545

    message

---
+++ mit.js
@@ -0,0 +1,20 @@
+let i=1;
+do {
+    console.log("Dipak");
+    i++
+} while(i<=5);
+
+
+let i=1;
+while(i<=5){
+    console.log("dipak");
+    i++;
+
+}
+for(let i=1; i<=5; i++) 
+{
+    console.log("dipak");
+}
+for(let i=0; i<=100; i++){
+    console.log("i")
+}
